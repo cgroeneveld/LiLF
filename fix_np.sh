dae1b06f6f699fb4bee3cfb864f8c11dd4ad0766
Christian Groeneveld <groeneveld@student18.strw.leidenuniv.nl>
2024-04-22 11:52:41 +0200

    removed helper file

--- fix_np.sh
+++
@@ -1,5 +0,0 @@
-find . -type f -name '*.py' -exec sed -i 's/np\.float16/float/g' {} \;
-find . -type f -name '*.py' -exec sed -i 's/np\.float/float/g' {} \;
-find . -type f -name '*.py' -exec sed -i 's/np\.int/int/g' {} \;
-find . -type f -name '*.py' -exec sed -i 's/np\.complex/complex/g' {} \;
-find . -type f -name '*.py' -exec sed -i 's/np\.bool/bool/g' {} \;
